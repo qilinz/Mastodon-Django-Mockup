ccea96e1916a6ff2ca36f6868fb8a3fd4eb38cc6
qilinz <zhangqilinz@outlook.com>
2022-02-08 17:48:12 +0100

    Change secrete key to environment variable

--- otter/otter/settings.py
+++ otter/otter/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 import os
+from dotenv import load_dotenv
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-c8-30$yt*(okwp0y6@*9(7nvvqf-sg8ol2+mg4&gy3dy2*@1p$'
+load_dotenv()
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
